b5d5a4b35603e77c31dbb1c3eb5d0ea830cfe0a4
Gabriel Gomes <gggabriel368@gmail.com>
2026-02-05 12:55:23 -0300

    Exercicio parcialmente pronto

---
+++ Pensionato-Exercicio-Vetores/Pensionato-Exercicio-Vetores/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+A dona de um pensionato possui dez quartos para alugar para estudantes,
+sendo esses quartos identificados pelos números 0 a 9.
+Quando um estudante deseja alugar um quarto, deve-se registrar o nome
+e email deste estudante.
+Fazer um programa que inicie com todos os dez quartos vazios, e depois
+leia uma quantidade N representando o número de estudantes que vão
+alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos
+N estudantes. Para cada registro de aluguel, informar o nome e email do
+estudante, bem como qual dos quartos ele escolheu (de 0 a 9). Suponha
+que seja escolhido um quarto vago. Ao final, seu programa deve imprimir
+um relatório de todas ocupações do pensionato, por ordem de quarto,
+conforme exemplo.
+*/
+
+using Pensionato_Exercicio_Vetores;
+
+Console.WriteLine("1 - consultar");
+Console.WriteLine("2 - registrar");
+Console.Write("escolha uma opcao: ");
+int op = int.Parse(Console.ReadLine());
+
+int numeroEstudantesParaCadastrar = 0;
+int numeroDeQuartos = 10;
+Reserva[] reservas = new Reserva[numeroDeQuartos];
+
+switch (op)
+{
+    case 1:
+        functionConsulta();
+        break;
+    case 2:
+        functionRegister();
+        break;
+    default:
+        Console.WriteLine("opcao inexistente");
+        break;
+}
+
+int functionConsulta()
+{
+    Console.WriteLine("Listagem dos registrados");
+    return 1;
+}
+
+Reserva[] functionRegister()
+{
+    Console.WriteLine("Preencha o formulario");
+    Console.WriteLine();
+    Console.WriteLine("Quantos estudantes: ");
+    numeroEstudantesParaCadastrar = int.Parse(Console.ReadLine());
+    
+    for(int i = 0; i < numeroEstudantesParaCadastrar; i++)
+    {
+        Console.WriteLine("Nome:");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Email:");
+        string email = Console.ReadLine();
+        Console.WriteLine("Quarto:");
+        int numeroQuarto = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        reservas[numeroQuarto] = new Reserva
+        {
+            Nome = nome,
+            Email = email,
+            NumeroQuarto = numeroQuarto
+        };
+    }
+
+    Console.WriteLine("Quartos cadastrados: ");
+    for(int i = 0; i<numeroDeQuartos; i++)
+    {
+        if (reservas[i] != null)
+        {
+            Console.WriteLine(reservas[i].ToString());
+        }
+    }
+    
+    return reservas;
+}
---
+++ Pensionato-Exercicio-Vetores/Pensionato-Exercicio-Vetores/Reserva.cs
@@ -0,0 +1,14 @@
+﻿namespace Pensionato_Exercicio_Vetores
+{
+    internal class Reserva
+    {
+        public int NumeroQuarto { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return NumeroQuarto + ": " + Nome + ", " + Email;
+        }
+    }
+}

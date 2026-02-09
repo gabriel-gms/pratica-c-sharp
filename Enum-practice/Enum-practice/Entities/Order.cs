16be6fb2843a33788af408551e1d94a22e0063e5
Gabriel Gomes <gggabriel368@gmail.com>
2026-02-09 11:40:33 -0300

    First exercices to Enum

---
+++ Enum-practice/Enum-practice/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using Enum_practice.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enum_practice.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public override string ToString()
+        {
+            return $"{Id}, {Moment}, {Status}";
+        }
+    }
+}
---
+++ Enum-practice/Enum-practice/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Crie um uma strutura enum e relacione ela com um atributo da classe
+Order na instanciação.
+*/
+
+using Enum_practice.Entities;
+using Enum_practice.Entities.Enums;
+
+Order order = new Order
+{
+    Id = 1,
+    Moment = DateTime.Now,
+    Status = OrderStatus.PendingPayment
+};
+
+Console.WriteLine(order.ToString());
